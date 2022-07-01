24c5ab9954ac2e0ebce2db0516658822a54bd68c
Alexander Borsuk <170263+biodranik@users.noreply.github.com>
2022-07-01 23:02:29 +0200

    Updated to version 14.0.6

--- Formula/clang-tidy.rb
+++ Formula/clang-tidy.rb
@@ -1,8 +1,8 @@
 class ClangTidy < Formula
   desc "Linting tools for C, C++ and Obj-C"
   homepage "https://clang.llvm.org/extra/clang-tidy/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/llvm-project-14.0.0.src.tar.xz"
-  sha256 "35ce9edbc8f774fe07c8f4acdf89ec8ac695c8016c165dd86b8d10e7cba07e23"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/llvm-project-14.0.6.src.tar.xz"
+  sha256 "8b3cfd7bc695bd6cea0f37f53f0981f34f87496e79e2529874fd03a2f9dd3a8a"
   # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
   license "Apache-2.0" => { with: "LLVM-exception" }
   version_scheme 1
@@ -14,11 +14,11 @@ class ClangTidy < Formula
     regex(%r{href=.*?/tag/llvmorg[._-]v?(\d+(?:\.\d+)+)}i)
   end
 
-  bottle do
-    root_url "https://github.com/fmenezes/homebrew-tap/releases/download/clang-tidy-14.0.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "3289b3b396c30d1fa61aee141e667596ffd93cabaae8d394f23c43046366d33c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d580aecb3245079a9e497bbee99eaced2225346aefc248a5dfbbde4e28b86abc"
-  end
+#  bottle do
+#    root_url "https://github.com/biodranik/homebrew-tap/releases/download/clang-tidy-14.0.6"
+#    sha256 cellar: :any_skip_relocation, big_sur:      "3289b3b396c30d1fa61aee141e667596ffd93cabaae8d394f23c43046366d33c"
+#    sha256 cellar: :any_skip_relocation, x86_64_linux: "d580aecb3245079a9e497bbee99eaced2225346aefc248a5dfbbde4e28b86abc"
+#  end
 
   depends_on "cmake" => :build
 
